e40037f0fc38d2c820431b28cab5fa9d0560d6ef
krishnakarki195 <krishna.karki195@gmail.com>
2020-04-01 20:47:06 -0500

    Movie API project setup complete

---
+++ src/main/java/com/galvanize/entities/Movie.java
@@ -0,0 +1,13 @@
+package com.galvanize.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+
+
+}
---
+++ src/test/java/com/galvanize/entities/MovieTest.java
@@ -0,0 +1,16 @@
+package com.galvanize.entities;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+
+
+
+}
