d4e3cc14151cb6ec952466abaa364988143634c5
krishnakarki195 <krishna.karki195@gmail.com>
2020-04-01 20:53:00 -0500

    JPA repository added

---
+++ src/main/java/com/galvanize/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.galvanize.repositories;
+
+import com.galvanize.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Long, Movie> {
+}
