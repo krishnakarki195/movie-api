653f29fe5647752b6ea0e8e7ede17fcbbe27bb71
krishnakarki195 <krishna.karki195@gmail.com>
2020-04-01 20:50:15 -0500

    Movie Entity added with properties and MovieTest complete

--- src/main/java/com/galvanize/entities/Movie.java
+++ src/main/java/com/galvanize/entities/Movie.java
@@ -8,6 +8,108 @@ import java.util.List;
 @Table(name = "movies")
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name="imdb_id")
+    private String imdbId;
+
+    @Column(name="actors")
+    @ElementCollection(targetClass=String.class)
+    private List<String> actors;
+
+    @Column(name= "director")
+    private String director;
+
+    @Column(name ="title")
+    private String title;
+
+    @Column(name="year")
+    private String year;
+
+    @Column(name="released")
+    private Date releasedDate;
+
+    public Movie(){}
+
+
+    public Movie(String imdbId, List<String> actors, String director, String title, String year, Date releasedDate) {
+        this.imdbId = imdbId;
+        this.actors = actors;
+        this.director = director;
+        this.title = title;
+        this.year = year;
+        this.releasedDate = releasedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public List<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<String> actors) {
+        this.actors = actors;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public Date getReleasedDate() {
+        return releasedDate;
+    }
+
+    public void setReleasedDate(Date releasedDate) {
+        this.releasedDate = releasedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", imdbId='" + imdbId + '\'' +
+                ", actors=" + actors +
+                ", director='" + director + '\'' +
+                ", title='" + title + '\'' +
+                ", year='" + year + '\'' +
+                ", releasedDate=" + releasedDate +
+                '}';
+    }
 
 }
--- src/test/java/com/galvanize/entities/MovieTest.java
+++ src/test/java/com/galvanize/entities/MovieTest.java
@@ -11,6 +11,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
 
+    @Test
+    public void movieObjDefaultConstructorTest(){
+        Movie movie = new Movie();
+        assertNotNull(movie);
+    }
 
+    @Test
+    public void movieObjParameterConstructorTest(){
+        String imdb_id = "imdb_123";
+        List<String> actors = new ArrayList<>();
+        String director = "Someone";
+        String title = "Movie Title";
+        String year = "2016";
+        Date releasedDate = Date.valueOf(LocalDate.of(1962, 11, 16));
+        Movie movie = new Movie(imdb_id,actors,director,title,year,releasedDate);
+        assertNotNull(movie);
+        assertEquals("2016", movie.getYear());
+    }
 
 }
