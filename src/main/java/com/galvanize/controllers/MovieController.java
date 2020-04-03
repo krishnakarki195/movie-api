c41ee14063ad1882438f0325c0b3838d791824c3
krishnakarki195 <krishna.karki195@gmail.com>
2020-04-02 20:09:16 -0500

    Complete MovieController class endpints and MovieControllerTest tests

--- src/main/java/com/galvanize/controllers/MovieController.java
+++ src/main/java/com/galvanize/controllers/MovieController.java
@@ -1,4 +1,41 @@
 package com.galvanize.controllers;
 
+import com.galvanize.entities.Movie;
+import com.galvanize.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
 public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+    @GetMapping(value = "/api/movies")
+    public List<Movie> getAllMovies(){
+        return movieService.getAll();
+    }
+
+    @PostMapping(value = "/api/movies")
+    public Movie save(Movie movie){
+        return movieService.save(movie);
+    }
+
+    @GetMapping(value = "/api/movies/{id}")
+    public Movie getById(@PathVariable("id") Long id){
+        return movieService.getById(id).get();
+    }
+
+    @DeleteMapping("/api/movies/{id}")
+    public boolean deleteById(@PathVariable("id") Long id){
+        return movieService.deleteById(id);
+    }
+
+    @PatchMapping("/api/movies/{id}")
+    public Movie updateMovieTitleById(@PathVariable("id") Long id, @RequestParam("title") String title){
+        return movieService.updateMovieTitle(id,title);
+    }
+
 }
--- src/test/java/com/galvanize/controllers/MovieControllerTest.java
+++ src/test/java/com/galvanize/controllers/MovieControllerTest.java
@@ -1,16 +1,34 @@
 package com.galvanize.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.entities.Movie;
 import com.galvanize.services.MovieService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
@@ -20,11 +38,96 @@ class MovieControllerTest {
     @Autowired
     MockMvc mvc;
 
-    @Autowired
+    @MockBean
     MovieService movieService;
 
     @Autowired
-    ObjectMapper mapper;
+    ObjectMapper objectMapper;
+
+    Movie movie;
+
+    @BeforeEach
+    public void setupTestData(){
+        List<String> m1_actors = Arrays.asList("Dean Cain", "Robin Givens", "Tamara Goodwin", "Matt Mercer");
+        List<String> m1_director = Arrays.asList("James Kondelik", "Jon Kondelik");
+        movie = new Movie("tt3417334",
+                m1_actors,
+                m1_director,
+                "Airplane vs. Volcano",
+                "2014",
+                Date.valueOf(LocalDate.of(2014, 03, 28)), "5");
+        movie.setId(1L);
+    }
+
+    @Test
+    public void saveMovieApiTest() throws Exception{
+
+        String url = "/api/movies";
+
+        when(movieService.save(ArgumentMatchers.any(Movie.class))).thenReturn(movie);
+
+        mvc.perform(post(url).content(objectMapper.writeValueAsString(movie))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(movie.getId()))
+                .andExpect(jsonPath("$.year").value(movie.getYear()))
+                .andDo(print());
+    }
+
+    @Test
+    public void findAllMovieApiTest() throws Exception {
+        String url = "/api/movies";
+
+        when(movieService.getAll()).thenReturn(Arrays.asList(movie));
+
+        mvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(greaterThanOrEqualTo(1))))
+                .andDo(print());
+    }
+
+    @Test
+    public void findByIdMovieApiTest() throws Exception {
+        String url = "/api/movies/1";
+
+        when(movieService.getById(ArgumentMatchers.any(Long.class))).thenReturn(java.util.Optional.ofNullable(movie));
+
+        mvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(movie.getId()))
+                .andExpect((jsonPath("$.title").value(movie.getTitle())))
+                .andDo(print());
+    }
+
+    @Test
+    public void deleteByIdMovieApiTest() throws Exception {
+        String url = "/api/movies/1";
+
+        when(movieService.deleteById(ArgumentMatchers.any(Long.class))).thenReturn(Boolean.valueOf("true"));
+
+        mvc.perform(delete(url)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void updateMovieTitleByIdApiTest() throws Exception {
+        String url = "/api/movies/1";
+        String newTitle = "new title goes here";
+
+        when(movieService.updateMovieTitle(ArgumentMatchers.any(Long.class),ArgumentMatchers.any(String.class))).thenReturn(movie);
+
+        mvc.perform(patch(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("title",newTitle))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(movie.getTitle()))
+                .andExpect(jsonPath("$.year").value(movie.getYear()))
+                .andDo(print());
+    }
 
 
 }
